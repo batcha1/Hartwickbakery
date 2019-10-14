02ec5608e5b2eb7a9cfce815ba14bdf99a3bb2a9
Aly <batcha@hartwick.edu>
2019-10-14 09:57:38 -0400

    Update

---
+++ Bakeryproject.py
@@ -0,0 +1,12 @@
+# Aly Batch
+# 10.14.19
+# Hartwick Bakery
+
+cookies = []
+candy = []
+
+def cookie_input():
+    for choco in range(0, 6):
+       var = input("")
+       choco += 1
+       print(var)
