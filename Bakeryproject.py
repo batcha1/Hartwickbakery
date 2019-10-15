5152766538c0dcae67ea0a58e91183e874c89afb
Aly <batcha@hartwick.edu>
2019-10-15 09:59:56 -0400

    Step 1 with print statements for better flow.

--- Bakeryproject.py
+++ Bakeryproject.py
@@ -5,9 +5,23 @@
 cookies = []
 candy = []
 
+
 def cookie_input():
     for choco in range(0, 6):
-       value = int(input("Monthly Sale:"))
-       cookies.append(value)
+        value = int(input(f"Monthly Sale for month {choco+1}:"))
+        cookies.append(value)
 
+
+def candy_input():
+    for trix in range(0, 6):
+        value = int(input(f"Monthly Sale for month {trix+1}:"))
+        candy.append(value)
+
+
+print("Here we are going to input the monthly sales we have had for cookies sold at the Hartwick Bakery.")
+print("Please enter the values for each month and then press enter each time you do. This will bring you to the next line.")
 cookie_input()
+print("**********")
+
+print("Now let's do the same thing for candy sales.")
+candy_input()
